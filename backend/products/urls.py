662a3989f0ccca7f1b11b6d36c52e7291d9ddcba
SGGM <gleb_somov@mail.ru>
2023-05-01 16:37:02 +0300

    mixins mixing =)

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,13 +4,15 @@ from .views import (
     product_detail_view, \
     product_list_create_view, \
     product_update_view, \
-    product_delete_view
+    product_delete_view, \
+    product_mixin_view
+    
 )
 
 
 urlpatterns = [
-    path('', product_list_create_view),
+    path('', product_mixin_view),
     path('<int:pk>/update/', product_update_view),
     path('<int:pk>/delete/', product_delete_view),
-	path('<int:pk>/', product_detail_view),
+	path('<int:pk>/', product_mixin_view),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -60,6 +60,38 @@ product_delete_view = ProductDeleteAPIView.as_view()
 #     # lookup_field = 'pk'
 
 # list_detail_view = ProductListAPIView.as_view()
+
+
+class ProductMixinView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+    ):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_fileds = 'pk'
+    
+    def get(self, request, *args, **kwargs):
+        print(args, kwargs)
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = 'single view cool stuff'
+        serializer.save(content=content)
+    
+product_mixin_view = ProductMixinView.as_view()
+
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
