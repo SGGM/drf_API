c3b14b5faa481aad2864d1a08d90af69458dad8d
SGGM <gleb_somov@mail.ru>
2023-05-01 15:44:29 +0300

    delete, update views added

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import product_detail_view, product_list_create_view, product_alt_view
+from .views import (
+    product_detail_view, \
+    product_list_create_view, \
+    product_update_view, \
+    product_delete_view
+)
 
 
 urlpatterns = [
     path('', product_list_create_view),
+    path('<int:pk>/update/', product_update_view),
+    path('<int:pk>/delete/', product_delete_view),
 	path('<int:pk>/', product_detail_view),
 ]
